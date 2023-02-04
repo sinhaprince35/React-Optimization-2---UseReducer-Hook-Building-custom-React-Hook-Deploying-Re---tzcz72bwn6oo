7da436c92cec1da7b16e611ee3550d5c7cabb72b
Anand Dubey(Aadi) <115421478+ananddubey1999@users.noreply.github.com>
2023-02-04 16:50:01 +0530

    Update function.js

--- src/function.js
+++ src/function.js
@@ -1,16 +1,23 @@
+
 const calculateAllPrimes = (number) => {
-  let primes = [];
-  for (let i = 2; i < number; i++) {
-    let prime = true;
-    for (let j = 2; j < i; j++) {
-      if (i % j === 0) {
-        prime = false;
-        break;
+  let primes = new Array(number + 1).fill(true);
+  primes[0] = false;
+  primes[1] = false;
+
+  for (let i = 2; i * i <= number; i++) {
+    if (primes[i] === true) {
+      for (let j = i * i; j <= number; j += i) {
+        primes[j] = false;
       }
     }
-    if (prime) primes.push(i);
   }
-  return primes;
+
+  let result = [];
+  for (let i = 2; i <= number; i++) {
+    if (primes[i]) result.push(i);
+  }
+
+  return result;
 };
 
-export default calculateAllPrimes
+export default calculateAllPrimes;
